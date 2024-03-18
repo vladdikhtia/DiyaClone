afb3f2888c529ff83fabdc7d2f77800ee3661c7a
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-18 12:08:33 +0100

    Initial Commit

---
+++ DiyaClone/DiyaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiyaCloneApp.swift
+//  DiyaClone
+//
+//  Created by Vladyslav Dikhtiaruk on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiyaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
